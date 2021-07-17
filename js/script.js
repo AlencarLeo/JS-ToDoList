a6603d1010d5c21ac1db2a61e31fa34d8b9b708b
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2021-07-17 15:36:28 -0300

    more updates

--- js/script.js
+++ js/script.js
@@ -8,10 +8,34 @@ btn.addEventListener('click',()=>{
     paragraph.classList.add('paragraph-style');
     paragraph.innerText = toDo.value.toUpperCase();
     toDo.value = "";
+
+    let btn = document.createElement('button');
+    toDoList.appendChild(btn);
+    btn.classList.add('btn-style', 'remove');
+    btn.innerText = "delete".toLocaleUpperCase();
+
+    let btn2 = document.createElement('button');
+    toDoList.appendChild(btn2);
+    btn2.classList.add('btn-style', 'do-it');
+    btn2.innerText = "done".toLocaleUpperCase();
+
+    btn2.addEventListener('click',()=>{
+        paragraph.style.textDecoration = "line-through";
+    });
+
+    btn.addEventListener('click',()=>{
+        toDoList.removeChild(paragraph);
+        toDoList.removeChild(btn);
+        toDoList.removeChild(btn2);
+    });
+
     paragraph.addEventListener('click',()=>{
         paragraph.style.textDecoration = "line-through";
     });
+
     paragraph.addEventListener('dblclick',()=>{
         toDoList.removeChild(paragraph);
+        toDoList.removeChild(btn);
+        toDoList.removeChild(btn2);
     });
 });
